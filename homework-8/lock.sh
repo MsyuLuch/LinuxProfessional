8f203b2e8f0ddb97f7eee64b6c4f9a91f48dfda9
Svetlana Mashkina <mashkina.syu@chita.tgk-14.com>
2021-09-20 08:47:29 +0900

    Update README.md

---
+++ homework-8/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOCKFILE=/tmp/lockfile
+
+if [[ -f $LOCKFILE ]]
+then
+  echo "Lockfile exists, try again later..." >&2
+  exit 1
+fi
+  echo "Process is running ( PID: $$ )" > $LOCKFILE
+  trap 'rm -f $LOCKFILE' EXIT
+  sh ./script_test.sh access.log 8 5 all
+  rm -f $LOCKFILE
+  trap - EXIT
+
